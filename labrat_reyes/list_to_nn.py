2420c05c124e67bdc296a50003e56862651e1575
marcus-reyes <marcus.lopez.reyes@gmail.com>
2019-10-18 09:10:34 +0800

    Base code for creating MLP layers from a list

---
+++ labrat_reyes/list_to_nn.py
@@ -0,0 +1,16 @@
+slf_list = [20,20,10]
+
+
+def createmodel(slf_list,inputsize):
+    fclist = [] 
+    #idea from amc
+    for i, size in enumerate(slf_list):
+        #First layer
+        if i == 0:
+            fclist.append(nn.Linear(inputsize, size))
+        else:
+            fclist.append(nn.Linear(slf_list[i-1], size))
+
+    net = nn.Sequential(*fclist).to(device)
+    print(net)
+    return net
